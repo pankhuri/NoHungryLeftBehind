3989678e7832073db53a1faaee467c6366822d17
pankhuri <pankhuri@quovantis.com>
2014-10-18 07:33:21 +0530

    Initial commit

---
+++ no_hungry_left_behind/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NoHungryLeftBehind::Application.config.secret_key_base = 'beafeeec8ed2114680f00ebc1c8a3e2f178d1dab6189f02cb83601a9004f548be469efc3381b70eb7c505fed80e73fc3066a1e840d33a49e49b9e5a553904ece'
