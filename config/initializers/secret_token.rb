397f1df0b41812f1b830ddbd1d9ab75b941dd306
shanky-munjal <shanky.munjal@quovantis.com>
2014-10-18 08:34:10 +0530

    the beginning of something great

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+R14Team265::Application.config.secret_key_base = '5ae86989f21e6c4331ce36fc1379264147c4ce870b67a4d18f85bfe6dad757f95554702e4685f5af97b6a3ee319c94b6054d290ee8c2bc07a194588887da28b0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+R14Team265::Application.config.session_store :cookie_store, key: '_r14-team-265_session'
