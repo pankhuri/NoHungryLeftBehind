f3a10e05d85409854f33552884579b9eb32824b6
pankhuri <pankhuri@quovantis.com>
2014-10-18 18:07:03 +0530

    Add custom image for markers

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 1.2'
 
 gem 'gmaps4rails'
+gem 'pry-byebug'
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,11 +1,22 @@
 class LocationsController < ApplicationController
   
   def index
-    # @locations = Location.all
-#     @hash = Gmaps4rails.build_markers(@locations) do |location, marker|
-#       marker.lat location.latitude
-#       marker.lng location.longitude
-#     end
+    #@locations = Location.all
+    @locations = [{ lat: 43, lng: 3.5},
+    { lat: 45, lng: 4},
+    { lat: 49, lng: 4},
+    { lat: 51, lng: 3.5}]
+    @hash = Gmaps4rails.build_markers(@locations) do |location, marker|
+      marker.lat location[:lat]
+      marker.lng location[:lng]
+      debugger
+      marker.picture({
+                  :url => "https://maps.google.com/mapfiles/kml/shapes/parking_lot_maps.png",
+                  :width   => 32,
+                  :height  => 32
+                  })
+      marker.json({:title => "DSdsdf"})
+    end
   end
   
 end
