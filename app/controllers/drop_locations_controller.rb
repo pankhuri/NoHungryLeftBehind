34523a90911a554340719e83aacb55fb5600ccf3
shanky-munjal <shanky.munjal@quovantis.com>
2014-10-20 03:02:47 +0530

    * add clas for green color

--- app/controllers/drop_locations_controller.rb
+++ app/controllers/drop_locations_controller.rb
@@ -1,8 +1,9 @@
 class DropLocationsController < ApplicationController
 	respond_to :json, :html, :js
 	def update
-		@drop_location = DropLocation.where("latitude like '%?%' and longitude like '%?%'", drop_location_params[:latitude], drop_location_params[:longitude]).first()
-		@drop_location.update_column(is_passed: true )			
+		@drop_location = DropLocation.where("latitude like '%?%' and longitude like '%?%'", drop_location_params[:latitude].to_f, drop_location_params[:longitude].to_f).first()
+		@drop_location.update_attributes(is_passed: true )
+		render json: @drop_location
 	end	
 
 	def drop_location_params
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 	def indicator_icon(location)
 		if location.is_passed?
-			"indicator-icon-active-green"
+			"indicator-icon indicator-icon-active-green"
 		else
 			"indicator-icon"
 		end
